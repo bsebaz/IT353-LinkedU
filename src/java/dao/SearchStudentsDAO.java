fa1de25eab4a0c0634b5536a570ed4c5153542fe
bsebaz <baileysebastian@gmail.com>
2018-11-27 18:21:41 -0600

    Lets users search the

--- src/java/dao/SearchStudentsDAO.java
+++ src/java/dao/SearchStudentsDAO.java
@@ -31,17 +31,28 @@ public class SearchStudentsDAO implements DAOInterface {
                 pstmt = DBConn.prepareStatement( insertString );
             }
             else{
-                searchTerm = searchTerm.trim();
-                String[] name = searchTerm.split(" ");
-                insertString = "SELECT * FROM LinkedUDB.students WHERE firstName LIKE %?% AND lastName LIKE %?%";
                 
-                // Note the use of a diff class, called PreparedStatement
-                pstmt = DBConn.prepareStatement( insertString );
+                if (searchTerm.contains(" ")){
+                    searchTerm = searchTerm.trim();
+                    String[] name = searchTerm.split(" ");
+                    insertString = "SELECT * FROM LinkedUDB.students WHERE UPPER(firstName) LIKE ? AND UPPER(lastName) LIKE ?";
 
-                pstmt.setString( 1, name[0]);
-                pstmt.setString( 2, name[1]);
+                    // Note the use of a diff class, called PreparedStatement
+                    pstmt = DBConn.prepareStatement( insertString );
+                    pstmt.setString( 1, "%"+name[0].toUpperCase()+"%");
+                    pstmt.setString( 2, "%"+name[1].toUpperCase()+"%");
+                }
+                else {
+                    searchTerm = searchTerm.trim();
+                    insertString = "SELECT * FROM LinkedUDB.students WHERE UPPER(firstName) LIKE ? OR UPPER(lastName) LIKE ?";
+
+                    // Note the use of a diff class, called PreparedStatement
+                    pstmt = DBConn.prepareStatement( insertString );
+                    pstmt.setString( 1, "%"+searchTerm.toUpperCase()+"%");
+                    pstmt.setString( 2, "%"+searchTerm.toUpperCase()+"%");
+                }
             }
-            
+            String stmt = pstmt.toString();
             ResultSet rs = pstmt.executeQuery();
             
             while(rs.next()){
