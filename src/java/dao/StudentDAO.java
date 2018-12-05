864cb7e94acd0a39cf981a3d997427f00b8aebb5
McHugh, Michael <mmchug1@ilstu.edu>
2018-12-04 22:56:07 -0600

    Added methods to load only names, used for the nav bar

--- src/java/dao/StudentDAO.java
+++ src/java/dao/StudentDAO.java
@@ -7,50 +7,67 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import model.Student;
-import model.User;
 
 /**
  *
  * @author kchris4
  */
-public class StudentDAO implements DAOInterface, java.io.Serializable{
-    
-    public static boolean insertStudent(Student student)
-    {
-                String myDB = "jdbc:derby://localhost:1527/LinkedUDB";// connection string
-                Connection DBConn = null;
-                Statement stmt = null;
-                
-                String insertString = "INSERT INTO LINKEDUDB.Students(FIRSTNAME, LASTNAME, AGE, SCHOOL, YEARGRADUATED, GPA) "
+public class StudentDAO implements DAOInterface, java.io.Serializable {
+
+    public static boolean insertStudent(Student student) {
+        String myDB = "jdbc:derby://localhost:1527/LinkedUDB";// connection string
+        Connection DBConn = null;
+        Statement stmt = null;
+
+        String insertString = "INSERT INTO LINKEDUDB.Students(FIRSTNAME, LASTNAME, AGE, SCHOOL, YEARGRADUATED, GPA) "
                 + "VALUES('" + student.getFirstName() + "','"
-                                    + student.getLastName() + "','"
-                                    + student.getAge() + "',"
-                                    + student.getSchool() + "',"
-                                    + student.getGraduationYear() + "',"
-                                    + student.getGpa() + "')";
-                
-                
-                try {
-                   DBConn = DriverManager.getConnection(myDB, "itkstu", "student");
-                   //stmt = DBConn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
-                   System.out.println(insertString);
-                   stmt = DBConn.createStatement();
-                   stmt.executeUpdate(insertString);
-                   
-                }
-                catch(Exception e){
-                    
-                    e.printStackTrace();
-              
-                    System.out.println("EXCEPTION: unable to insert user");
-                    return false;
-                }
-                
-            
-            
+                + student.getLastName() + "','"
+                + student.getAge() + "',"
+                + student.getSchool() + "',"
+                + student.getGraduationYear() + "',"
+                + student.getGpa() + "')";
+
+        try {
+            DBConn = DriverManager.getConnection(myDB, "itkstu", "student");
+            //stmt = DBConn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
+            System.out.println(insertString);
+            stmt = DBConn.createStatement();
+            stmt.executeUpdate(insertString);
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+            System.out.println("EXCEPTION: unable to insert user");
+            return false;
+        }
+
         return true;
-     }
-    
+    }
+
+    public String getStudentName(int userID) {
+        String query = "SELECT FIRSTNAME, LASTNAME FROM LINKEDUDB.STUDENTS WHERE ACCOUNTID = ?";
+        String name = "Profile";
+        try (Connection db = connect()) {
+            PreparedStatement pstmt = null;
+            pstmt = db.prepareStatement(query);
+            pstmt.setInt(1, userID);
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                name = rs.getString("firstName") + " " + rs.getString("lastName");
+            }
+            rs.close();
+            pstmt.close();
+            db.close();
+        } catch (SQLException e) {
+            System.out.println("Failed to get student's name");
+            System.out.println(e.getLocalizedMessage());
+        }
+        return name;
+    }
 }
